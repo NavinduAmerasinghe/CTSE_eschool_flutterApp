9ee1966ec31368b1dd23cf1e6b70985294cba447
Piyumi0 <it20158804@my.sliit.lk>
2023-03-22 12:05:50 +0530

    add student screen controller add student binding add student binding deleted

--- lib/presentation/add_student_screen/binding/add_student_binding.dart
+++
@@ -1,9 +0,0 @@
-import '../controller/add_student_controller.dart';
-import 'package:get/get.dart';
-
-class AddStudentBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut(() => AddStudentController());
-  }
-}
