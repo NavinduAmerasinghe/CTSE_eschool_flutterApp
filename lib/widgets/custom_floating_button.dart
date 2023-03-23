f21fad21722257cc3b7b5d91fd60a50e9c2dfc27
Buddhini-123 <buddhiniup123@gmail.com>
2023-03-23 19:34:32 +0530

    added design to floating button

--- lib/widgets/custom_floating_button.dart
+++ lib/widgets/custom_floating_button.dart
@@ -1,4 +1,10 @@
 import 'package:eschool/core/app_export.dart';
 import 'package:flutter/material.dart';
 
-
+class CustomFloatingButton extends StatelessWidget {
+  CustomFloatingButton(
+      {this.shape,
+      this.variant,
+      this.alignment,
+      this.margin,
+      );
