841d77fd5856ad2cefc8b5c3625995ea7ebe8ceb
Buddhini-123 <buddhiniup123@gmail.com>
2023-03-24 09:43:19 +0530

    custom image required varibles declared

--- lib/widgets/custom_image_view.dart
+++ lib/widgets/custom_image_view.dart
@@ -5,3 +5,17 @@ import 'dart:io';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+
+class CustomImageView extends StatelessWidget {
+  ///[url] is required parameter for fetching network image
+  String? url;
+
+  ///[imagePath] is required parameter for showing png,jpg,etc image
+  String? imagePath;
+
+  ///[svgPath] is required parameter for showing svg image
+  String? svgPath;
+
+  ///[file] is required parameter for fetching image file
+  File? file;
+
