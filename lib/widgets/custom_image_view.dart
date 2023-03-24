e7a67f9390d39d354c8c7cf2cb596bdc507cea8d
Buddhini-123 <buddhiniup123@gmail.com>
2023-03-24 13:17:32 +0530

    firebase authorization issue fixed

--- lib/widgets/custom_image_view.dart
+++ lib/widgets/custom_image_view.dart
@@ -62,6 +62,9 @@ class CustomImageView extends StatelessWidget {
   Widget _buildWidget() {
     return Padding(
       padding: margin ?? EdgeInsets.zero,
-      
+      child: InkWell(
+        onTap: onTap,
+        child: _buildCircleImage(),
+      ),
     );
   }
